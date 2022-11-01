841932848e480103cb0d4d340e8ef60f10825130
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-11-01 08:28:28 +0200

    Add project files.

---
+++ Student Services Management App API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Student_Services_Management_App_API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
