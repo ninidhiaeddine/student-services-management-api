27f5d354e481a44667648900382cee6db0578405
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-11-25 02:29:48 +0200

    Added database context class for connection

---
+++ Student Services Management App API/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Services_Management_App_API.Models;
+
+namespace Student_Services_Management_App_API;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Residence> Residences { get; set; }
+    public DbSet<TimeSlot> TimeSlots { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL(
+            "Server=eu-cdbr-west-03.cleardb.net;Database=heroku_c89291c3e0d17eb;Uid=b79a7b90d0e9fd;Pwd=1ca8623f;");
+    }
+}
