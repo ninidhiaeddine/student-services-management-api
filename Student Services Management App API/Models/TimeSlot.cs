37cc99492671b4508e10d726988fd986371ed2b3
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-11-26 12:34:36 +0200

    Fixed variable type of ServiceType in TimeSlot.cs

--- Student Services Management App API/Models/TimeSlot.cs
+++ Student Services Management App API/Models/TimeSlot.cs
@@ -5,7 +5,7 @@ namespace Student_Services_Management_App_API.Models;
 public class TimeSlot
 {
     [Key] public int PK_TimeSlot { get; set; }
-    public string ServiceType { get; set; }
+    public int ServiceType { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public int MaximumCapacity { get; set; }
