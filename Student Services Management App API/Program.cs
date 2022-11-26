12b1442c67eeb26c3c2416cac987258ed76a5985
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-11-26 11:52:28 +0200

    Confugured authentication

--- Student Services Management App API/Program.cs
+++ Student Services Management App API/Program.cs
@@ -1,3 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Student_Services_Management_App_API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// establish connection with database:
+var connectionString = Environment.GetEnvironmentVariable("ASPNETCORE_CONNECTIONSTRING");
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseMySQL(connectionString));
+
+// configure authentication
+var jwtIssuer = Environment.GetEnvironmentVariable("ASPNETCORE_JWTISSUER");
+var jwtKey = Environment.GetEnvironmentVariable("ASPNETCORE_JWTKEY");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        });
 
 var app = builder.Build();
 
@@ -18,6 +44,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
