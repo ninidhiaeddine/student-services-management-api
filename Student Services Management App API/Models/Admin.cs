fb5186347c5c9469be82bb095987eaee8d14eb07
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-12-15 22:22:03 +0200

    Inherited from IdentityUser for Users

--- Student Services Management App API/Models/Admin.cs
+++ Student Services Management App API/Models/Admin.cs
@@ -1,14 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Student_Services_Management_App_API.Models;
 
-public class Admin
+public class Admin : IdentityUser
 {
     [JsonPropertyName("PK_Admin")] [Key] public int PK_Admin { get; set; }
 
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
     public string HashedPassword { get; set; } = string.Empty;
 }
--- Student Services Management App API/Models/Student.cs
+++ Student Services Management App API/Models/Student.cs
@@ -1,17 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Student_Services_Management_App_API.Models;
 
-public class Student
+public class Student : IdentityUser
 {
-    [JsonPropertyName("PK_Student")]
-    [Key] 
-    public int PK_Student { get; set; }
+    [JsonPropertyName("PK_Student")] [Key] public int PK_Student { get; set; }
 
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
     public int StudentId { get; set; }
     public int Gender { get; set; } // 0 = Male, 1 = Female
     public int IsDorms { get; set; } // 0 = False, 1 = True
